4809b7812835d7a22f9bde6a6394f3263e272882
Nuha Nishat <49707671+nuhanishat@users.noreply.github.com>
2020-06-23 22:14:09 -0700

    Add files via upload
    Contains object pose estimation files

---
+++ ObjectPoseEstimation/camera_cal.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2 
+import glob
+import cv2.aruco as aruco
+import math
+
+#termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,30,0.001)
+
+#prepare object points,like (0,0,0), (2,0,0) ...., (6,5,0)
+object_p = np.zeros((6*7,3), np.float32)
+object_p[:,:2] = np.mgrid[0:7, 0:6].T.reshape(-1,2)
+
+#Array to store object points and image points from all the images
+object_points = [] # 3D point in real world space
+img_points = [] # 2D points in image plane
+
+#Square size : This is the square size of the checkerboard boxes in meters
+# size = 0.025 #m or 25 cm 
+
+
+images = glob.glob('/home/nuha/kinova_ws/src/traj-control/images/*.jpg')
+
+for fname in images:
+	img = cv2.imread(fname)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# Find chess board corners
+	ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+
+	# If found, add object points, image points (after refining them)
+	if ret == True:
+		object_points.append(object_p)
+
+		corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+		img_points.append(corners2)
+
+		#Draw and display the corners
+		img = cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+		cv2.imshow('img',img)
+		cv2.waitKey(500)
+
+
+# Get all the relevant matrices
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, img_points, gray.shape[::-1],None,None)
+
+print('Camera Matrix:')
+print(mtx)
+np.save('camera_mtx.npy', mtx)
+
+print('Distortion Matrix:')
+print(dist)
+np.save('dist_mtx.npy', dist)
+
---
+++ ObjectPoseEstimation/pose_estimation.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import String, Float32
+import numpy as np
+import cv2.aruco as aruco
+import sys
+
+
+class ImageProcessor():
+	def __init__(self):
+		# Object pose publisher
+		self.pose_pub = rospy.Publisher("object_pose", Float32, queue_size=10)
+		# Marker ID publisher 
+		self.marker_pub = rospy.Publisher('marker_id', String, queue_size=10)
+
+		#cv bridge class
+		self.bridge = CvBridge()
+
+		# This is the image message subcriber. Change the topic to your camera topic (most likely realsense)
+		self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.get_object_pose)
+		
+	# Callback for image processing
+	def get_object_pose(self, img_msg):
+
+ 
+		# Aruko Marker Info
+		marker_size = 5 #cm 
+
+		# Marker IDs
+		ee_marker_id = 0  # end-effector
+		obj_marker_id = 1 # object 
+
+		# Get the saved camera and distortion matrices from calibration
+		mtx = np.load('/home/nuha/kinova_ws/src/traj-control/src/camera_mtx.npy') # camera matrix
+		dist = np.load('/home/nuha/kinova_ws/src/traj-control/src/dist_mtx.npy')  # distortion matrix
+
+		# Define Aruco Dictionary 
+		aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_1000)
+		parameters = aruco.DetectorParameters_create()
+
+		#Lists for storing marker positions
+		ee_marker = []
+		obj_marker = []
+
+	
+		#Convert ros image message to opencv image
+
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+		#Convert in gray scale
+		gray = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+
+		#Find markers in the image 
+		corners, ids, rejected = aruco.detectMarkers(image=gray, dictionary=aruco_dict, parameters=parameters, cameraMatrix=mtx, distCoeff=dist)
+
+		if np.all(ids != None):
+
+			rvec, tvec, __ = aruco.estimatePoseSingleMarkers(corners,marker_size, mtx, dist)
+			
+			for i in range(ids.size):
+				#Draw reference frame for the marker
+				
+				# Save end-effector marker pose
+				if ids[i] == ee_marker_id:
+					ee_marker = tvec[i]
+
+				# Save object marker pose
+				if ids[i] == obj_marker_id:
+					obj_marker = tvec[i]
+
+				aruco.drawAxis(cv_image, mtx, dist, rvec[i], tvec[i], 10)
+
+			#Draw Markers
+			aruco.drawDetectedMarkers(cv_image, corners, ids)
+
+			# print('obj: ', obj_marker)
+			# print('ee: ', ee_marker)
+
+			if len(ee_marker) > 0 and len(obj_marker) > 0 :
+				# Get the pose of object with respect to end-effector 
+				delta_pose = [(x - y)/100.0 for x, y in zip(obj_marker, ee_marker)] # Prints in meters. Default is cm.
+				print(delta_pose)
+
+				self.pose_pub.publish(delta_pose)
+				self.marker_pub.publish(str(obj_marker_id))
+			else: 
+				print("No Marker Found")
+
+			
+			
+
+		#Display
+		cv2.imshow('Window', cv_image)
+		cv2.waitKey(3)
+			
+	
+def main(args):
+	f = ImageProcessor()
+	rospy.init_node('pose_estimation', anonymous=True)
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+	cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+	main(sys.argv)
