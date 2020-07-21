ad36a0aaa91cc76212d9111a7bfe6b17412b1b39
graspinglab <pareshsoni92@gmail.com>
2020-07-21 13:58:57 -0700

    Added Reset Mech to reset of env and go to home steps and Included call in launch file

--- grasp_classifier_individual_sim_2_real/src/kinova_gripper_env.py
+++ grasp_classifier_individual_sim_2_real/src/kinova_gripper_env.py
@@ -27,7 +27,7 @@ import rospy
 from sensor_msgs.msg import JointState
 from kinova_msgs.msg import FingerPosition, KinovaPose
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import String, Float32, Float32MultiArray
+from std_msgs.msg import String, Float32, Float32MultiArray, Int32
 
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -69,12 +69,14 @@ class KinovaGripper_Env:
         self.object_pose_sub = rospy.Subscriber('/object_pose', Float32, object_pose_callback, queue_size=1)
         self.marker_id_sub = rospy.Subscriber('/marker_id', String, marker_id_callback, queue_size=1)
         self.finger_dist_sub = rospy.Subscriber('/finger_dist', Float32, finger_dist_callback, queue_size=1)
-        self.finger_pose_sub = rospy.Subscriber('/finger_pose', Float32, finger_pose_callback, queue_size=1)        
+        self.finger_pose_sub = rospy.Subscriber('/finger_pose', Float32, finger_pose_callback, queue_size=1)
+        self.reset_check_sub = rospy.Subscriber('/sim2real/reset_status', Int32, reset_check_callback, queue_size=1)        
         
         ###Publisher###
         self.finger_command_pub = rospy.Publisher('/sim2real/finger_command', FingerPosition, queue_size=1)
         self.joint_angle_command_pub = rospy.Publisher('/sim2real/joint_angle_command', JointState, queue_size=1)
         self.obs_pub = rospy.Publisher('/sim2real/obs', Float32MultiArray, queue_size=1)
+        self.reset = rospy.Publisher('/sim2real/reset', Int32, queue_size=1)
         
     ### Finger Position in Radians ###
     def get_joint_states(self): 
@@ -224,7 +226,22 @@ class KinovaGripper_Env:
         joint_state_goal = self.joint_angle7
         self.move_arm_joint_angle(joint_state_goal)
         
+    def go_to_home(self):
 
+        joint_state_goal = self.joint_angle7
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle6
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle5
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle4
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle3
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle2
+        self.move_arm_joint_angle(joint_state_goal)
+        joint_state_goal = self.joint_angle1
+        self.move_arm_joint_angle(joint_state_goal)
 
     def move_arm_joint_angle(self, joint_angle):
         self.joint_pos_goal = JointState()
@@ -237,7 +254,15 @@ class KinovaGripper_Env:
 
         rospy.set_param('exec_done', "false")
 
+    def reset(self):
+        reset_command = 1
+        self.reset.publish(reset_command)
+        while not self.reset_done:
+            rospy.sleep(0.1)
 
+        self.reset_done = 0
+
+        self.go_to_home()
 
 
     def seed(self, seed=None):
@@ -264,6 +289,8 @@ class KinovaGripper_Env:
     def finger_dist_callback(self, msg):
         self.finger_dist_list = msg          
               
+    def reset_check_callback(self, msg):
+        self.reset_done = msg
 
     def finger_pose_callback(self, msg):
         self.finger_pose_list = msg
