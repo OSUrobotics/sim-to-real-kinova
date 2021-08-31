e73438a8823efaf4e1700cbd6f215b37ab0a6e78
mechagodzilla <jimzers>
2021-08-31 10:28:29 -0700

    modify simulator params for cubem, cylinderm, cone1m, vase1m

--- src/sim-to-real-kinova-master/openai_gym_kinova/src/gym_kinova_gripper/envs/kinova_description/test.py
+++ src/sim-to-real-kinova-master/openai_gym_kinova/src/gym_kinova_gripper/envs/kinova_description/test.py
@@ -4,6 +4,10 @@ import os
 rel_dirname = os.path.dirname(__file__)
 
 filepath = os.path.join(rel_dirname, "j2s7s300_end_effector_v1_CubeM.xml")
+filepath = os.path.join(rel_dirname, "j2s7s300_end_effector_v1_CylinderM.xml")
+filepath = os.path.join(rel_dirname, "j2s7s300_end_effector_v1_Cone1M.xml")
+filepath = os.path.join(rel_dirname, "j2s7s300_end_effector_v1_Vase1M.xml")
+
 print('filepath: ', filepath)
 
 model = load_model_from_path(filepath)
